b0f14ff048e41814426364fe9d5b55a561cfc99e
dewang-jandhan <dewangsahuji@gmail.com>
2026-05-26 23:11:40 +0530

    Dockerising the app

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from app.routers import auth , prompts , social , image , user , collection
 from app.database import engine
 from app.database import Base
 
-app = FastAPI(title="Prompt Platform API", version="1.0")
+app = FastAPI(title="Prompt Platform API", version="1.0" , root_path="/api")
 
 
 @app.on_event("startup")
@@ -20,17 +20,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-
-# # Mount the auth router
-# app.include_router(auth.router, prefix="/auth")
-# app.include_router(prompts.router , prefix="/prompts")
-# app.include_router(social.router)
-# app.include_router(image.router, prefix="/images")  # ✅ prefix="/images" is set inside the router
-# app.include_router(user.router, prefix="/users")    # prefix="/users"  set inside router
-# app.include_router(collection.router, prefix="/collections")  # prefix="/collections"  set inside router
-
 
 app.include_router(auth.router,    prefix="/api/auth")
 app.include_router(prompts.router, prefix="/api/prompts")
